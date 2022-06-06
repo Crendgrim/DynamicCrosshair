baabf2d25bd27edc5d4f635e4771a3b0430be122
Crendgrim <Crendgrim@users.noreply.github.com>
2022-06-06 13:08:25 +0200

    Hack: Patch MythicMounts to not crash by bad cast if checking for breeding item

---
+++ src/main/java/mod/crend/dynamiccrosshair/DynamicCrosshairMixinPlugin.java
@@ -0,0 +1,51 @@
+package mod.crend.dynamiccrosshair;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class DynamicCrosshairMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        String[] split = mixinClassName.split("\\.compat\\.mixin\\.");
+        if (split.length == 2) {
+            String modid = split[1].split("\\.")[0];
+            return FabricLoader.getInstance().isModLoaded(modid);
+        }
+        return true;
+    }
+
+    @Override
+    public void onLoad(String mixinPackage) {
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
