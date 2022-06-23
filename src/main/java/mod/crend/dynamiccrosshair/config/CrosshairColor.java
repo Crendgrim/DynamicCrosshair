08929fd28ea44ba3b22f48d2126b2eaf96b77df2
Crendgrim <Crendgrim@users.noreply.github.com>
2022-06-23 13:18:13 +0200

    Color bleed change broke default custom colour

--- src/main/java/mod/crend/dynamiccrosshair/config/CrosshairColor.java
+++ src/main/java/mod/crend/dynamiccrosshair/config/CrosshairColor.java
@@ -1,7 +1,17 @@
 package mod.crend.dynamiccrosshair.config;
 
+import mod.crend.dynamiccrosshair.DynamicCrosshair;
+
 public record CrosshairColor(CrosshairConfigColor color, int customColor, boolean forced) {
 	public int getColor() {
-		return (color == CrosshairConfigColor.Custom ? customColor : color.color);
+		return switch (color) {
+			case Unchanged -> getDefaultColor();
+			case Custom -> customColor;
+			default -> color.color;
+		};
+	}
+	private static int getDefaultColor() {
+		CrosshairColor defaultColor = DynamicCrosshair.config.getColor();
+		return (defaultColor.color() == CrosshairConfigColor.Custom ? defaultColor.customColor : defaultColor.color.color);
 	}
 }
