3db3c9167846db25da6af7725e72d48f481cbdf1
Crendgrim <Crendgrim@users.noreply.github.com>
2023-01-13 22:31:13 +0100

    Prettier API loading

--- fabric/src/main/java/mod/crend/dynamiccrosshair/fabric/DynamicCrosshairFabric.java
+++ fabric/src/main/java/mod/crend/dynamiccrosshair/fabric/DynamicCrosshairFabric.java
@@ -4,6 +4,7 @@ import mod.crend.dynamiccrosshair.DynamicCrosshair;
 import mod.crend.dynamiccrosshair.api.DynamicCrosshairApi;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 
 public class DynamicCrosshairFabric implements ClientModInitializer {
 
@@ -11,11 +12,10 @@ public class DynamicCrosshairFabric implements ClientModInitializer {
     public void onInitializeClient() {
         DynamicCrosshair.init();
 
-        FabricLoader.getInstance().getEntrypointContainers(DynamicCrosshair.MOD_ID, DynamicCrosshairApi.class).forEach(entrypoint -> {
-            DynamicCrosshairApi apiImpl = entrypoint.getEntrypoint();
-            if (FabricLoader.getInstance().isModLoaded(apiImpl.getModId())) {
-                DynamicCrosshair.registerApi(apiImpl);
-            }
-        });
+        FabricLoader.getInstance().getEntrypointContainers(DynamicCrosshair.MOD_ID, DynamicCrosshairApi.class)
+                .stream()
+                .map(EntrypointContainer::getEntrypoint)
+                .filter(api -> FabricLoader.getInstance().isModLoaded(api.getModId()))
+                .forEach(DynamicCrosshair::registerApi);
     }
 }
